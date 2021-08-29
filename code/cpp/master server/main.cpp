9e8ba6ec2d736603e655e77ea9b31436cea58e13
Philippe <22506952+PhilJbt@users.noreply.github.com>
2021-08-29 18:20:50 +0200

    [MS] Nomenclature

---
+++ code/cpp/master server/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "synak.h"
+
+
+int main() {
+    std::cerr << "[START] " << SK_BUILDTIMESTAMP << std::endl;
+
+    SynakManager mngr; 
+    mngr.Launch();
+
+    std::cerr << "[STOP] " << std::endl;
+
+    return 0;
+}
